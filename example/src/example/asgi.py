bea14fb094f8a7b48e68771b6efa15034dc50def
Mikail <mikail-gh@pm.me>
2026-02-28 20:14:37 +0100

    Cleanup auto-generated files from django-admin

--- example/src/example/asgi.py
+++ example/src/example/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for example project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
