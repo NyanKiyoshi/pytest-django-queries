3650436f945e73e14c012356ab368ccfec6361c2
Mikail <mikail-gh@pm.me>
2026-03-01 16:22:35 +0100

    fix: pytest-django no longer being optional
    This makes the workaround from 06db7b800c51a89d4bcd200cbad1ce665da12087 work against installations that do not use `pytest-django` as our plugin does not require users to install it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,17 @@ import pytest
 
 pytest_plugins = "pytester"
 
+# Prevents autoloading plugins as they could impact the test results,
+# e.g., we could fail to detect that the plugin doesn't work if
+# pytest-django isn't installed (as it's an optional plugin)
+DEFAULT_PYTEST_FLAGS = [
+    "--disable-plugin-autoload",
+    "-p",
+    "xdist",
+    "-p",
+    "django_queries",
+]
+
 
 @pytest.fixture
 def valid_comparison_entries():
